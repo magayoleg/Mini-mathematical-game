f0f8814856bcc49497c2c77f83abe7c7b1b9cf0f
Oleg <server90@mail.ru>
2021-09-04 05:32:42 +0300

    finished fifth task

---
+++ bin/brain-even.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+function randomNumber() {
+  const array = [];
+  for (let i = 0; i < 3; i += 1) {
+    const valueArray = [];
+    valueArray.push(Math.round(Math.random() * 100));
+    if (valueArray[0] % 2) {
+      valueArray.push('no');
+    } else {
+      valueArray.push('yes');
+    }
+    array.push(valueArray);
+  }
+  return array;
+}
+
+function game() {
+  console.log('Answer "yes" if the number is even, otherwise answer "no".');
+  const numbers = randomNumber();
+  for (const [number, answer] of numbers) {
+    console.log(`Question: ${number}`);
+    const question = readlineSync.question(`Your answer: `);
+    if (question === answer) {
+      console.log('Correct!');
+    } else {
+      return console.log(`'yes' is wrong answer ;(.
+Correct answer was 'no'. Let's try again, Bill!`);
+    }
+  }
+  return console.log('Congratulations, Bill!');
+}
+export { game };
--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,8 @@
 #!/usr/bin/env node
 import * as user from '../src/cli.js';
+import * as game from './brain-even.js'
 
 console.log('Welcome to the Brain Games!');
 
 user.userName();
+game.game();
