d2f08b2baae96e68d6467b11c27b53c87bf655f6
Oleg <server90@mail.ru>
2021-09-06 01:10:31 +0300

    finished ninth task

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,7 @@
 import readlineSync from 'readline-sync';
 
-function userName() {
+export default function userName() {
   const name = readlineSync.question('May I have your name?');
   console.log(`Hello, ${name}!`);
   return name;
 }
-
-export { userName };
