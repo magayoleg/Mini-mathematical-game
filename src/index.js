5e186718c1517ba7b49f9c81cbf343f824b63bdd
Oleg <server90@mail.ru>
2021-09-07 21:50:00 +0300

    eslint-disable in index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 
+/* eslint-disable */
 export default function gameEngine(massage, callback) {
   console.log('Welcome to the Brain Games!');
   const name = readlineSync.question('May I have your name?');
